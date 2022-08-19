af7b7646366684af728a89b3782a3103d5bed97a
Elijah Hartvigsen <elijah.reed@hartvigsen.xyz>
2022-08-19 15:03:10 +0200

    ZeroCopyText: Add `zero_copy::text::int` parser with test suite

--- src/zero_copy/text.rs
+++ src/zero_copy/text.rs
@@ -16,6 +16,8 @@ pub trait Char: Sized {
     fn is_whitespace(&self) -> bool;
 
     fn is_digit(&self, radix: u32) -> bool;
+
+    fn is_zero(&self) -> bool;
 }
 
 impl Char for char {
@@ -44,6 +46,10 @@ impl Char for char {
     fn is_digit(&self, radix: u32) -> bool {
         char::is_digit(*self, radix)
     }
+
+    fn is_zero(&self) -> bool {
+        *self == '0'
+    }
 }
 
 impl Char for u8 {
@@ -71,6 +77,10 @@ impl Char for u8 {
 
     fn is_digit(&self, radix: u32) -> bool {
         char::from(*self).is_digit(radix)
+    }
+
+    fn is_zero(&self) -> bool {
+        *self == b'0'
     }
 }
 
@@ -136,6 +146,32 @@ where
         .filter(move |x: &I::Token| x.is_digit(radix))
         .repeated_slice()
         .at_least(1)
+        .map(C::from)
+}
+
+/// A parser that accepts a non-negative integer.
+///
+/// An integer is defined as a non-empty sequence of ASCII digits, where the first digit is non-zero or the sequence
+/// has length one.
+///
+/// The `radix` parameter functions identically to [`char::is_digit`]. If in doubt, choose `10`.
+pub fn int<'a, I, E, S, C>(radix: u32) -> impl Parser<'a, I, E, S, Output = C>
+where
+    I: Input + ?Sized + SliceInput,
+    I::Token: Char,
+    <I as SliceInput>::Slice: 'a,
+    E: Error<I>,
+    S: 'a,
+    C: From<&'a <I as SliceInput>::Slice>,
+{
+    primitive::any()
+        .filter(move |x: &I::Token| x.is_zero())
+        .repeated_slice()
+        .exactly(1)
+        .or(primitive::any()
+            .filter(move |x: &I::Token| x.is_digit(radix))
+            .repeated_slice()
+            .at_least(1))
         .map(C::from)
 }
 
@@ -253,6 +289,63 @@ mod tests {
                 .parse(input);
 
             assert_eq!(res, (Some(vec![123_i32, 456, 789, 000,]), Vec::new()));
+        }
+
+        #[test]
+        fn parses_int() {
+            let res = int::<_, (), (), _>(10).parse("0123456789");
+            assert_eq!(res, (Some("0".to_string()), Vec::new()));
+
+            let res = int::<_, (), (), _>(10).parse("123456789");
+            assert_eq!(res, (Some("123456789".to_string()), Vec::new()));
+        }
+
+        #[test]
+        fn parses_int_into_slice() {
+            let res = int::<_, (), (), _>(10).parse("0123456789");
+            assert_eq!(res, (Some("0"), Vec::new()));
+
+            let res = int::<_, (), (), _>(10).parse("123456789");
+            assert_eq!(res, (Some("123456789"), Vec::new()));
+        }
+
+        #[test]
+        fn parses_int_into_vec() {
+            let res = int::<_, (), (), _>(16).parse("0123456789ABCDEF");
+            assert_eq!(res, (Some(b"0".to_vec()), Vec::new()));
+
+            let res = int::<_, (), (), _>(16).parse("123456789ABCDEF");
+            assert_eq!(res, (Some(b"123456789ABCDEF".to_vec()), Vec::new()));
+        }
+
+        #[test]
+        fn parses_int_from_non_static() {
+            let string = String::from("f123456");
+            let res = int::<_, (), (), _>(7).parse(&string[1..]);
+            assert_eq!(res, (Some("123456"), Vec::new()));
+        }
+
+        #[test]
+        fn int_with_others() {
+            let input = "0 123 456 789 000";
+            let res = int::<_, (), (), &'_ str>(10)
+                .then_ignore(super::prelude::just(' '))
+                .repeated()
+                .collect()
+                .parse(input);
+
+            assert_eq!(res, (Some(vec!["0", "123", "456", "789"]), Vec::new()));
+
+            let input = "0 123 456 789 000";
+            let res = int::<_, (), (), _>(10)
+                .padded()
+                .map(|x: &str| x.parse())
+                .unwrapped()
+                .repeated()
+                .collect()
+                .parse(input);
+
+            assert_eq!(res, (Some(vec![0, 123_i32, 456, 789, 0, 0, 0]), Vec::new()));
         }
     }
 }
