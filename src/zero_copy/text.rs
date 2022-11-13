f4a2f1ed18a00465e846732ce32e6493ecbf87a9
Benoit de Chezelles <benoit.dechezelles@gmail.com>
2022-11-13 06:50:56 +0100

    Fix trait bounds for file src/zero_copy/text.rs

--- src/zero_copy/text.rs
+++ src/zero_copy/text.rs
@@ -69,17 +69,15 @@ pub struct Padded<A> {
     pub(crate) parser: A,
 }
 
-impl<'a, I, E, S, A> Parser<'a, I, E, S> for Padded<A>
+impl<'a, I, O, E, S, A> Parser<'a, I, O, E, S> for Padded<A>
 where
     I: Input + ?Sized,
     E: Error<I>,
     S: 'a,
     I::Token: Char,
-    A: Parser<'a, I, E, S>,
+    A: Parser<'a, I, O, E, S>,
 {
-    type Output = A::Output;
-
-    fn go<M: Mode>(&self, inp: &mut InputRef<'a, '_, I, E, S>) -> PResult<M, Self::Output, E> {
+    fn go<M: Mode>(&self, inp: &mut InputRef<'a, '_, I, E, S>) -> PResult<M, O, E> {
         inp.skip_while(|c| c.is_whitespace());
         let out = self.parser.go::<M>(inp)?;
         inp.skip_while(|c| c.is_whitespace());
