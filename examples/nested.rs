cf98b6b07a11fee49565c1b820a3724c69928b25
Joshua Barretto <joshua.s.barretto@gmail.com>
2023-02-23 16:36:04 +0000

    Simplified nested example

--- examples/nested.rs
+++ examples/nested.rs
@@ -1,4 +1,4 @@
-use chumsky::zero_copy::{input::WithContext, prelude::*};
+use chumsky::zero_copy::prelude::*;
 
 // This token is a tree: it contains within it a sub-tree of tokens
 #[derive(Clone, PartialEq, Debug)]
@@ -9,14 +9,14 @@ enum Token {
     Parens(Vec<Token>),
 }
 
-fn parser<'a>() -> impl Parser<'a, WithContext<(), &'a [Token]>, i64> {
+fn parser<'a>() -> impl Parser<'a, &'a [Token], i64> {
     recursive(|expr| {
         let num = select! { Token::Num(x) => *x };
         let parens = expr
             // Needed to ensure that we parse *all* of the nested tokens
             .then_ignore(end())
             // Here we specify how the parser should come up with the nested tokens
-            .nested_in(select! { Token::Parens(xs) => WithContext::new((), xs.as_slice()) });
+            .nested_in(select! { Token::Parens(xs) => xs.as_slice() });
 
         let atom = num.or(parens);
 
@@ -46,14 +46,12 @@ fn parser<'a>() -> impl Parser<'a, WithContext<(), &'a [Token]>, i64> {
 }
 
 fn main() {
+    // This token tree represents the expression `(2 + 3) * 4`
     let tokens = [
         Token::Parens(vec![Token::Num(2), Token::Add, Token::Num(3)]),
         Token::Mul,
         Token::Num(4),
     ];
 
-    assert_eq!(
-        parser().parse(WithContext::new((), &tokens)).into_result(),
-        Ok(20),
-    );
+    assert_eq!(parser().parse(&tokens).into_result(), Ok(20),);
 }
