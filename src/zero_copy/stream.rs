3255493e329069599ca2741b395a7368d65c6acf
Joshua Barretto <joshua.s.barretto@gmail.com>
2023-03-01 16:46:19 +0000

    Revert Stream constructor name

--- src/zero_copy/stream.rs
+++ src/zero_copy/stream.rs
@@ -17,13 +17,13 @@ impl<I: Iterator> Stream<I> {
     ///
     /// ```
     /// # use chumsky::zero_copy::{prelude::*, input::Stream};
-    /// let stream = Stream::from_tokens((0..10).map(|i| char::from_digit(i, 10).unwrap()));
+    /// let stream = Stream::from_iter((0..10).map(|i| char::from_digit(i, 10).unwrap()));
     ///
     /// let parser = text::digits::<_, _, extra::Err<Simple<_>>>(10).collect::<String>();
     ///
     /// assert_eq!(parser.parse(&stream).into_result().as_deref(), Ok("0123456789"));
     /// ```
-    pub fn from_tokens<J: IntoIterator<IntoIter = I>>(iter: J) -> Self {
+    pub fn from_iter<J: IntoIterator<IntoIter = I>>(iter: J) -> Self {
         Self {
             tokens: Cell::new((Vec::new(), Some(iter.into_iter()))),
         }
