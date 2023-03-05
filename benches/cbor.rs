5e62981bd9e30d5022028cbf890913b7326a4cba
Rune Tynan <runetynan@gmail.com>
2023-03-05 17:06:39 -0500

    Cbor improvements

--- benches/cbor.rs
+++ benches/cbor.rs
@@ -1,5 +1,3 @@
-use std::collections::HashMap;
-use std::hash::{Hash, Hasher};
 use std::hint::black_box;
 use criterion::{Criterion, criterion_group, criterion_main};
 
@@ -40,10 +38,10 @@ fn bench_cbor(c: &mut Criterion) {
         }
     });
 
-    // c.bench_function("cbor_serde_cbor", {
-    //     use serde_cbor::{from_slice, Value};
-    //     move |b| b.iter(|| black_box(from_slice::<Value>(black_box(JSON)).unwrap()))
-    // });
+    c.bench_function("cbor_serde_cbor", {
+        use serde_cbor::{from_slice, Value};
+        move |b| b.iter(|| black_box(from_slice::<Value>(black_box(CBOR)).unwrap()))
+    });
 
     // c.bench_function("cbor_pom", {
     //     let cbor = pom::cbor();
@@ -89,77 +87,18 @@ pub enum CborZero<'a> {
     Bytes(&'a [u8]),
     String(&'a str),
     Array(Vec<CborZero<'a>>),
-    Map(HashMap<CborZero<'a>, CborZero<'a>>),
+    Map(Vec<(CborZero<'a>, CborZero<'a>)>),
     Tag(u64, Box<CborZero<'a>>),
     SingleFloat(f32),
     DoubleFloat(f64),
 }
 
-impl<'a> Eq for CborZero<'a> {}
-
-impl<'a> Hash for CborZero<'a> {
-    fn hash<H: Hasher>(&self, state: &mut H) {
-        fn hash_map<H: Hasher, H1: Hash, H2: Hash>(map: &HashMap<H1, H2>, state: &mut H) {
-            for el in map {
-                el.hash(state)
-            }
-        }
-
-        match self {
-            CborZero::Int(i) => {
-                state.write_u8(0);
-                i.hash(state);
-            }
-            CborZero::Bytes(b) => {
-                state.write_u8(1);
-                b.hash(state);
-            }
-            CborZero::String(s) => {
-                state.write_u8(2);
-                s.hash(state);
-            }
-            CborZero::Array(a) => {
-                state.write_u8(3);
-                a.hash(state);
-            }
-            CborZero::Map(m) => {
-                state.write_u8(4);
-                hash_map(m, state);
-            }
-            CborZero::Tag(t, d) => {
-                state.write_u8(5);
-                t.hash(state);
-                d.hash(state);
-            }
-            CborZero::SingleFloat(f) => {
-                state.write_u8(6);
-                f.to_ne_bytes().hash(state);
-            }
-            CborZero::DoubleFloat(f) => {
-                state.write_u8(7);
-                f.to_ne_bytes().hash(state);
-            }
-            CborZero::Bool(b) => {
-                state.write_u8(8);
-                b.hash(state);
-            }
-            CborZero::Null => {
-                state.write_u8(9);
-            }
-            CborZero::Undef => {
-                state.write_u8(10);
-            }
-        }
-    }
-}
-
 mod chumsky_zero_copy {
-    use std::collections::HashMap;
     use chumsky::prelude::*;
     use crate::i64_from_bytes;
     use super::CborZero;
 
-    type Error<'a> = Rich<'a, u8>;
+    type Error<'a> = EmptyErr /*Rich<'a, u8>*/;
 
     fn int_out(slice: &[u8]) -> i64 {
         if slice.len() == 1 {
@@ -174,14 +113,14 @@ mod chumsky_zero_copy {
             let read_int = any().then_with_ctx(
                 any()
                     .repeated()
-                    .try_configure(|cfg, ctx, span| {
+                    .try_configure(|cfg, ctx, _| {
                         let info = *ctx & 0b1_1111;
                         let num = if info < 24 {
                             0
                         } else if info < 28 {
                             2usize.pow(info as u32 - 24)
                         } else {
-                            return Err(Error::custom(span, format!("Invalid argument: {}", info)))
+                            return Err(Error::empty() /*custom(span, format!("Invalid argument: {}", info))*/)
                         };
                         Ok(cfg.exactly(num))
                     })
@@ -233,15 +172,15 @@ mod chumsky_zero_copy {
                         .configure(|cfg, ctx| {
                             cfg.exactly(*ctx as usize)
                         })
-                        .collect::<HashMap<_, _>>()
+                        .collect::<Vec<_>>()
                         .map(CborZero::Map)
                 );
 
             let simple = |num: u8| any()
-                .try_map(move |n, span| if n & 0b1_1111 == num {
+                .try_map(move |n, _| if n & 0b1_1111 == num {
                     Ok(())
                 } else {
-                    Err(Error::custom(span, format!("Invalid simple identifier {}", n)))
+                    Err(Error::empty() /*custom(span, format!("Invalid simple identifier {}", n))*/)
                 });
 
             let float_simple = choice((
@@ -264,10 +203,10 @@ mod chumsky_zero_copy {
             ));
 
             let major = |num: u8| any()
-                .try_map(move |n, span| if (n >> 5) == num {
+                .try_map(move |n, _| if (n >> 5) == num {
                     Ok(())
                 } else {
-                    Err(Error::custom(span, format!("Invalid major version {}", n >> 5)))
+                    Err(Error::empty() /*custom(span, format!("Invalid major version {}", n >> 5))*/)
                 })
                 .rewind();
 
@@ -278,7 +217,7 @@ mod chumsky_zero_copy {
                 major(3).ignore_then(str),
                 major(4).ignore_then(array),
                 major(5).ignore_then(map),
-                major(6).ignore_then(end().try_map(|_, span| Err(Error::custom(span, "tag")))),
+                major(6).ignore_then(todo()),
                 major(7).ignore_then(float_simple),
             ))
         })
