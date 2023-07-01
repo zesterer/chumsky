b248ccbc4357a49f3aff4942e0be5612a4282330
Elijah Hartvigsen <elijah.reed@hartvigsen.xyz>
2023-07-01 12:21:34 +0200

    pratt: remove dead traits

--- src/pratt/state.rs
+++ src/pratt/state.rs
@@ -29,22 +29,3 @@ pub struct InfixPrefixPostfix<P1, P1O, P2, P2O, P3, P3O> {
     pub(crate) postfix: P3,
     pub(crate) phantom: PhantomData<(P1O, P2O, P3O)>,
 }
-
-/// DOC
-pub trait PrattOps: private::Sealed {}
-
-impl<P, PO> private::Sealed for Infix<P, PO> {}
-
-impl<P1, P2, P1O, P2O> private::Sealed for InfixPrefix<P1, P2, P1O, P2O> {}
-
-impl<P1, P2, P1O, P2O> private::Sealed for InfixPostfix<P1, P2, P1O, P2O> {}
-
-impl<P1, P1O, P2, P2O, P3, P3O> private::Sealed for InfixPrefixPostfix<P1, P1O, P2, P2O, P3, P3O> {}
-
-impl<P, PO> PrattOps for Infix<P, PO> {}
-
-impl<P1, P2, P1O, P2O> PrattOps for InfixPrefix<P1, P2, P1O, P2O> {}
-
-impl<P1, P2, P1O, P2O> PrattOps for InfixPostfix<P1, P2, P1O, P2O> {}
-
-impl<P1, P1O, P2, P2O, P3, P3O> PrattOps for InfixPrefixPostfix<P1, P1O, P2, P2O, P3, P3O> {}
