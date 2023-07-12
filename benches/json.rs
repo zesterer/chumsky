e2fb71861c3d51432d3122687972f0c7383eba7e
Ed Page <eopage@gmail.com>
2023-07-12 10:46:20 -0500

    refactor(bench): Move away from deprecated APIs

--- benches/json.rs
+++ benches/json.rs
@@ -346,24 +346,24 @@ mod nom {
 
 mod winnow {
     use winnow::{
-        branch::{alt, dispatch},
-        bytes::{any, none_of, one_of, tag, take_while0},
-        character::{digit0, digit1, escaped},
+        ascii::{digit0, digit1, escaped},
+        combinator::separated0,
+        combinator::{alt, dispatch},
         combinator::{cut_err, fail, opt, peek},
-        error::{Error, ParseError},
-        multi::separated0,
+        combinator::{preceded, separated_pair, terminated},
+        error::{InputError, ParserError},
         prelude::*,
-        sequence::{preceded, separated_pair, terminated},
+        token::{any, none_of, one_of, tag, take_while0},
     };
 
     use super::JsonZero;
     use std::str;
 
-    fn space<'a, E: ParseError<&'a [u8]>>(i: &'a [u8]) -> IResult<&'a [u8], &'a [u8], E> {
+    fn space<'a, E: ParserError<&'a [u8]>>(i: &'a [u8]) -> IResult<&'a [u8], &'a [u8], E> {
         take_while0(|c| b" \t\r\n".contains(&c)).parse_next(i)
     }
 
-    fn number<'a, E: ParseError<&'a [u8]>>(i: &'a [u8]) -> IResult<&'a [u8], f64, E> {
+    fn number<'a, E: ParserError<&'a [u8]>>(i: &'a [u8]) -> IResult<&'a [u8], f64, E> {
         (
             opt('-'),
             alt(((one_of("123456789"), digit0).void(), one_of('0').void())),
@@ -375,7 +375,7 @@ mod winnow {
             .parse_next(i)
     }
 
-    fn string<'a, E: ParseError<&'a [u8]>>(i: &'a [u8]) -> IResult<&'a [u8], &'a [u8], E> {
+    fn string<'a, E: ParserError<&'a [u8]>>(i: &'a [u8]) -> IResult<&'a [u8], &'a [u8], E> {
         preceded(
             '"',
             cut_err(terminated(
@@ -386,7 +386,7 @@ mod winnow {
         .parse_next(i)
     }
 
-    fn array<'a, E: ParseError<&'a [u8]>>(i: &'a [u8]) -> IResult<&'a [u8], Vec<JsonZero>, E> {
+    fn array<'a, E: ParserError<&'a [u8]>>(i: &'a [u8]) -> IResult<&'a [u8], Vec<JsonZero>, E> {
         preceded(
             '[',
             cut_err(terminated(
@@ -397,7 +397,7 @@ mod winnow {
         .parse_next(i)
     }
 
-    fn member<'a, E: ParseError<&'a [u8]>>(
+    fn member<'a, E: ParserError<&'a [u8]>>(
         i: &'a [u8],
     ) -> IResult<&'a [u8], (&'a [u8], JsonZero), E> {
         separated_pair(
@@ -408,7 +408,7 @@ mod winnow {
         .parse_next(i)
     }
 
-    fn object<'a, E: ParseError<&'a [u8]>>(
+    fn object<'a, E: ParserError<&'a [u8]>>(
         i: &'a [u8],
     ) -> IResult<&'a [u8], Vec<(&'a [u8], JsonZero)>, E> {
         preceded(
@@ -421,7 +421,7 @@ mod winnow {
         .parse_next(i)
     }
 
-    fn value<'a, E: ParseError<&'a [u8]>>(i: &'a [u8]) -> IResult<&'a [u8], JsonZero, E> {
+    fn value<'a, E: ParserError<&'a [u8]>>(i: &'a [u8]) -> IResult<&'a [u8], JsonZero, E> {
         preceded(
             space,
             dispatch!(peek(any);
@@ -438,11 +438,11 @@ mod winnow {
         .parse_next(i)
     }
 
-    fn root<'a, E: ParseError<&'a [u8]>>(i: &'a [u8]) -> IResult<&'a [u8], JsonZero, E> {
+    fn root<'a, E: ParserError<&'a [u8]>>(i: &'a [u8]) -> IResult<&'a [u8], JsonZero, E> {
         terminated(value, space).parse_next(i)
     }
 
-    pub fn json<'a>(i: &'a [u8]) -> IResult<&'a [u8], JsonZero, Error<&'a [u8]>> {
+    pub fn json<'a>(i: &'a [u8]) -> IResult<&'a [u8], JsonZero, InputError<&'a [u8]>> {
         root.parse_next(i)
     }
 }
