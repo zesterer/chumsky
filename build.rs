a7ecae426e40ac84bbf159a27cebae5d7c52cec1
stefnotch <brandmairstefan@gmail.com>
2023-09-26 21:35:06 +0200

    Only enable vergen on docsrs

--- build.rs
+++ build.rs
@@ -1,8 +1,20 @@
 use std::error::Error;
+#[cfg(feature = "docsrs")]
 use vergen::EmitBuilder;
 
 fn main() -> Result<(), Box<dyn Error>> {
+    emit_git_metadata()?;
+    Ok(())
+}
+
+#[cfg(feature = "docsrs")]
+fn emit_git_metadata() -> Result<(), Box<dyn Error>> {
     // Emit the instructions
     EmitBuilder::builder().all_git().emit()?;
+    Ok(())
+}
+
+#[cfg(not(feature = "docsrs"))]
+fn emit_git_metadata() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
