4092f9c230a4e7da751ecd189d85e609202dfa2b
Hedgehogo <lologlek8@gmail.com>
2025-07-03 11:39:48 +0300

    refactor: `try_map` unwrap an error if the child parser fails
    This will stop the spread of erroneous behavior and simplify debugging.

--- src/combinator.rs
+++ src/combinator.rs
@@ -659,12 +659,12 @@ where
         let span = inp.span_since(&before);
         let new_alt = inp.errors.alt.take();
 
+        inp.errors.alt = old_alt;
         match res {
             Ok(out) => {
                 match (self.mapper)(out, span) {
                     Ok(out) => {
                         // If successful, reinsert the original alt and then apply the new alt on top of it, since both are valid
-                        inp.errors.alt = old_alt;
                         if let Some(new_alt) = new_alt {
                             inp.add_alt_err(&new_alt.pos, new_alt.err);
                         }
@@ -673,18 +673,16 @@ where
 
                     Err(err) => {
                         // If unsuccessful, reinsert the original alt but replace the new alt with the mapper error (since it overrides it)
-                        inp.errors.alt = old_alt;
                         inp.add_alt_err(&before.inner, err);
                         Err(())
                     }
                 }
             }
 
-            Err(_) => {
-                inp.errors.alt = old_alt;
-                if let Some(new_alt) = new_alt {
-                    inp.add_alt_err(&new_alt.pos, new_alt.err);
-                }
+            Err(_) => {                
+                // Can't fail!
+                let new_alt = new_alt.unwrap();
+                inp.add_alt_err(&new_alt.pos, new_alt.err);
                 Err(())
             },
         }
