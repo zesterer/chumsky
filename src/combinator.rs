e818786cc68a4fab122b4779e5dc22e236a01af3
Zeichenreihe <67289119+zeichenreihe@users.noreply.github.com>
2025-12-19 17:34:25 +0000

    add `Parser::filter_map` (#925)
    * add `Parser::filter_map`
    
    The code is mostly just the implementation of `Parser::filter`, but
    taking an `Option<U>` from the condition.
    
    * Implement `Filter::go` by using `FilterMap`, add `FilterMap` to parsers guide
    
    * format changes using `cargo fmt`
    
    ---------
    
    Co-authored-by: zeichenreihe <zeichenreihe@noreply.codeberg.org>

--- src/combinator.rs
+++ src/combinator.rs
@@ -262,6 +262,48 @@ where
 
     #[inline(always)]
     fn go<M: Mode>(&self, inp: &mut InputRef<'src, '_, I, E>) -> PResult<M, O> {
+        (&self.parser)
+            .filter_map(|out| if (self.filter)(&out) { Some(out) } else { None })
+            .go::<M>(inp)
+    }
+
+    go_extra!(O);
+}
+
+/// See [`Parser::filter_map`].
+pub struct FilterMap<A, OA, F> {
+    pub(crate) parser: A,
+    pub(crate) filter_mapper: F,
+    #[allow(dead_code)]
+    pub(crate) phantom: EmptyPhantom<OA>,
+}
+
+impl<A: Copy, OA, F: Copy> Copy for FilterMap<A, OA, F> {}
+impl<A: Clone, OA, F: Clone> Clone for FilterMap<A, OA, F> {
+    fn clone(&self) -> Self {
+        Self {
+            parser: self.parser.clone(),
+            filter_mapper: self.filter_mapper.clone(),
+            phantom: EmptyPhantom::new(),
+        }
+    }
+}
+
+impl<'src, I, O, E, A, OA, F> Parser<'src, I, O, E> for FilterMap<A, OA, F>
+where
+    I: Input<'src>,
+    E: ParserExtra<'src, I>,
+    A: Parser<'src, I, OA, E>,
+    F: Fn(OA) -> Option<O>,
+{
+    #[doc(hidden)]
+    #[cfg(feature = "debug")]
+    fn node_info(&self, scope: &mut debug::NodeScope) -> debug::NodeInfo {
+        debug::NodeInfo::Filter(Box::new(self.parser.node_info(scope)))
+    }
+
+    #[inline(always)]
+    fn go<M: Mode>(&self, inp: &mut InputRef<'src, '_, I, E>) -> PResult<M, O> {
         let found = inp.peek_maybe();
         let before = inp.cursor();
         // Remove the pre-inner alt, to be reinserted later so we always preserve it
@@ -274,19 +316,22 @@ where
         inp.errors.alt = old_alt;
         match res {
             Ok(out) => {
-                if (self.filter)(&out) {
-                    // If successful, reinsert the original alt and then apply the new alt on top of it, since both are valid
-                    if let Some(new_alt) = new_alt {
-                        inp.add_alt_err(&new_alt.pos, new_alt.err);
+                match (self.filter_mapper)(out) {
+                    Some(mapped) => {
+                        // If successful, reinsert the original alt and then apply the new alt on top of it, since both are valid
+                        if let Some(new_alt) = new_alt {
+                            inp.add_alt_err(&new_alt.pos, new_alt.err);
+                        }
+                        Ok(M::bind(|| mapped))
                     }
-                    Ok(M::bind(|| out))
-                } else {
-                    // If unsuccessful, reinsert the original alt but replace the new alt with the "something else" error (since it overrides it)
-                    let expected = [DefaultExpected::SomethingElse];
-                    // TODO: Use something more detailed than the next token as the found
-                    let err = E::Error::expected_found(expected, found, span);
-                    inp.add_alt_err(&before.inner, err);
-                    Err(())
+                    None => {
+                        // If unsuccessful, reinsert the original alt but replace the new alt with the "something else" error (since it overrides it)
+                        let expected = [DefaultExpected::SomethingElse];
+                        // TODO: Use something more detailed than the next token as the found
+                        let err = E::Error::expected_found(expected, found, span);
+                        inp.add_alt_err(&before.inner, err);
+                        Err(())
+                    }
                 }
             }
 
